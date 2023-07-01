f364566a07f1f65c7a57e8bbfb91ca2eea7fc508
TophUwO <tophuwo01@gmail.com>
2023-07-01 18:50:23 +0200

    REWAMP exception handling
    - Now print banner on startup
    - Improve error messages

--- srcproj/src/sukajan.py
+++ srcproj/src/sukajan.py
@@ -12,12 +12,34 @@
 # This file serves as the main entry point for the application.
 
 # imports
+import logging
+import os
 import client as sj_client
 
 
 # Start main loop.
 if __name__ == '__main__':
-    with sj_client.SukajanClient() as tmp_client:
-        pass
+    print(
+        '''######################################################################\n'''
+        '''# Project:    Sukajan Bot v0.1                                       #\n'''
+        '''# Author:     Sukajan One-Trick <tophuwo01@gmail.com>                #\n'''
+        '''# Description:                                                       #\n'''
+        '''#   a bot for the KirikoMains subreddit for advanced custom          #\n'''
+        '''#   features required by the moderation team                         #\n'''
+        '''#                                                                    #\n'''
+        '''# (C) 2023 Sukajan One-Trick. All rights reserved.                   #\n'''
+        '''######################################################################\n'''
+        '''\n\n'''
+    )
+
+    try:
+        with sj_client.SukajanClient() as tmp_client:
+            pass
+    except Exception as tmp_e:
+        logging.critical(f'Fatal error: {tmp_e}')
+
+        os.abort()
+
+    print('Shutdown ...\n')
 
 
